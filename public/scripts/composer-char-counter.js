37137556fd9583a66d8886fb566079107cdde860
Travis <travisliu708@gmail.com>
2022-09-12 11:45:04 -0700

    adjust char counter to show remaining char available using jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  
+
+$(document).ready(function () {
+  console.log('jquery ready');
+  $("#tweet-text").on("input", function () {
+    if (140 - $("#tweet-text").val().length < 0) {
+      $(this).parent().find(".counter").css("color", "red");
+    };
+    if (140 - $("#tweet-text").val().length >= 0) {
+      $(this).parent().find(".counter").css("color", "#545149");
+    };
+
+    $(this).parent().find(".counter").val(140 - $("#tweet-text").val().length);
+  });
 });
