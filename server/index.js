dd4125aa95b10b4f4699ff44747259cb501d1482
Travis <travisliu708@gmail.com>
2022-09-11 23:43:39 -0700

    set up jquery and add js file for character counter

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const morgan = require('morgan');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -31,6 +32,7 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
