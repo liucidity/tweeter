1498b782e7c30b72ceb5aceecd1754cb3ef85ddb
Travis <travisliu708@gmail.com>
2022-09-13 12:51:09 -0700

    add alert if form text area is empty or contains too many characters

--- public/scripts/form-submission.js
+++ public/scripts/form-submission.js
@@ -2,15 +2,20 @@ $(function () {
   console.log('jquery ready');
   $(".tweet-entry").submit(function (event) {
     event.preventDefault();
-    $.ajax({
-      url: "/tweets",
-      data: $(this).serialize(),
-      method: "POST",
+    if ($("#tweet-text").val().length > 140) {
+      alert("Hum is too long. Please limit to 140 characters");
+    } else if ($("#tweet-text").val() === null
+      || $("#tweet-text").val() === "") {
+      alert("Hum cannot be empty");
+    } else {
+      $.ajax({
+        url: "/tweets",
+        data: $(this).serialize(),
+        method: "POST",
 
-    }).done(function (data) {
-      console.log(data);
-    });
-
+      }).done(function (data) {
+        console.log(data);
+      });
+    }
   });
-
 });
