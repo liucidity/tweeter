6dfb83d5ac374897b15349f2b301c37ac7afadf6
Travis <travisliu708@gmail.com>
2022-09-13 12:37:43 -0700

    add loadtweets to get data from tweets object. adjust createTweetElement to display timeago using timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,6 @@
 const renderTweets = function (tweets) {
   for (let tweet of tweets) {
     $(function () {
-      console.log(tweet);
       $('#tweets-container').append(createTweetElement(tweet));
     });
   }
@@ -30,7 +29,7 @@ const createTweetElement = function (tweet) {
   </p>
   <footer>
     <time>
-      ${tweet.created_at}
+      ${timeago.format(tweet.created_at)}
     </time>
     <div class="social-icons">
       <i class="fa-solid fa-flag"></i>
@@ -42,31 +41,11 @@ const createTweetElement = function (tweet) {
   return tweets;
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
-renderTweets(data);
+const loadTweets = function () {
+  $(function () {
+    $.get('/tweets', (data) => {
+      renderTweets(data);
+    });
+  });
+};
+loadTweets();
