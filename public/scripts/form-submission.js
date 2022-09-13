33224d83ea28a5c1db18ff18454c31c85f118cc3
Travis <travisliu708@gmail.com>
2022-09-13 16:14:56 -0700

    remove alert to show post error, replace with error box that slides down if an error occurs

--- public/scripts/form-submission.js
+++ public/scripts/form-submission.js
@@ -1,15 +1,29 @@
 // const { loadTweets } = require("./client");
 
 $(function () {
-  console.log('jquery ready');
+  $(".error-message").hide();
   $(".tweet-entry").submit(function (event) {
     event.preventDefault();
     if ($("#tweet-text").val().length > 140) {
-      alert("Hum is too long. Please limit to 140 characters");
+
+      $(".error-message").html(`
+      <i class="fa-solid fa-triangle-exclamation"></i>
+      Hum is too long! Please keep it to 140 characters
+        <i i class= "fa-solid fa-triangle-exclamation" ></i > `);
+
+      $(".error-message").slideDown();
     } else if ($("#tweet-text").val() === null
       || $("#tweet-text").val() === "") {
-      alert("Hum cannot be empty");
+
+      $(".error-message").html(`
+      <i class="fa-solid fa-triangle-exclamation"></i>
+      Hum cannot be empty
+        <i i class= "fa-solid fa-triangle-exclamation" ></i > `);
+
+      $(".error-message").slideDown();
+
     } else {
+      $(".error-message").slideUp();
       $.ajax({
         url: "/tweets",
         data: $(this).serialize(),
